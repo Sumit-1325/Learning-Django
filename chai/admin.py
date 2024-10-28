f651dbfb2032b2ec04fd5c455d6622a0badf8e51
Sumit 1325 <sumit13thakur124@gmail.com>
2024-10-29 00:56:33 +0530

    Learned Django model relationships

--- chai/admin.py
+++ chai/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import chaivariety
+from .models import chaivariety, chaicertificate, chaiReview, store
 
 # Register your models here.
-admin.site.register(chaivariety)
+
+class ChaiReviewInline(admin.TabularInline):
+    model = chaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_added', 'type')
+    inlines = [ChaiReviewInline]
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('certificate_no', 'certificate')
+
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_variety',)
+
+admin.site.register(chaivariety, ChaiVarietyAdmin)
+admin.site.register(store, storeAdmin)
+admin.site.register(chaicertificate, ChaiCertificateAdmin)
--- chai/models.py
+++ chai/models.py
@@ -1,24 +1,54 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-
-class chaivariety(models.Model):         #chaivariety creates structure for database
+class chaivariety(models.Model):
     CHAI_TYPE_CHOICES = [
-        ('ML','MASALA'),
-        ('GR','GARLIC') ,
-        ('KI','KIWI'),
-        ('PL','PLAIN'),
-        ('SP','SPICY'),
-        ('EL','ELACHI')
+        ('ML', 'MASALA'),
+        ('GR', 'GARLIC'),
+        ('KI', 'KIWI'),
+        ('PL', 'PLAIN'),
+        ('SP', 'SPICY'),
+        ('EL', 'ELACHI')
     ]
-
-    
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='chais/')
-    date_added = models.DateTimeField(default=timezone.now) #date_added
+    date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES, default='PL')
     description = models.TextField(default='no description')
+    
     def __str__(self):
         return self.name
+
+# One to Many Relationship
+class chaiReview(models.Model):
+    chai = models.ForeignKey(chaivariety, on_delete=models.CASCADE,related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return f'{self.user.username} Review for {self.chai.name}'
+    
+    #Many To Many Relationship
+
+class store(models.Model):
+        name = models.CharField(max_length=100)
+        location = models.CharField(max_length=100)
+        chai_variety = models.ManyToManyField(chaivariety , related_name="store")
+
+        def __str__(self):
+         return self.name
+
+
+#one to one relationship
+
+class chaicertificate(models.Model):
+    certificate = models.OneToOneField(chaivariety, on_delete=models.CASCADE,related_name="certificate")
+    certificate_no = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+     return f'{self.chai.name} Certificate {self.certificate_no}'
